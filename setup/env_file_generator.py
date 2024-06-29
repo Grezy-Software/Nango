1b215d1a47a474f71c3b330f0db506fc9479d53f
Xenepix <Firenix.nex@gmail.com>
2024-06-29 12:22:37 +0200

    feat(setup): .env load global environ varialbes

--- setup/env_file_generator.py
+++ setup/env_file_generator.py
@@ -4,6 +4,7 @@ Run the script with the .env name as the first argument.
     backend/config/secret_generator.py <env_name>
 """  # noqa: INP001
 
+import os
 import sys
 from pathlib import Path
 
@@ -15,7 +16,16 @@ def _generate_key(multiplier: int = 1) -> str:
     return "".join([Fernet.generate_key().decode()[:-1] for _ in range(multiplier)])
 
 
-def _make_folder(env_name: str) -> Path:
+def get_or_generate_key(name: str, multiplier: int = 1) -> str:
+    """Return the key if it exists, otherwise generate a new key."""
+    result = os.environ.get(name, None)
+    if result is None:
+        result = _generate_key(multiplier)
+    return result
+
+
+def make_folder(env_name: str) -> Path:
+    """Build .envs folder."""
     root_dir: Path = Path(__file__).resolve(strict=True).parent.parent.joinpath("backend")
     if not root_dir.is_dir():
         root_dir.mkdir(parents=True)
@@ -27,7 +37,7 @@ def _make_folder(env_name: str) -> Path:
 
 def build_postgres_env(env_name: str) -> None:
     """Build .postgres file of .env."""
-    folder: Path = _make_folder(env_name)
+    folder: Path = make_folder(env_name)
     path: Path = folder.joinpath(".postgres")
     if path.is_file():
         return
@@ -37,8 +47,8 @@ def build_postgres_env(env_name: str) -> None:
     content += 'POSTGRES_HOST = "postgres"\n'
     content += "POSTGRES_PORT = 5432\n"
     content += 'POSTGRES_DB = "napse_developer_toolkit"\n'
-    content += f'POSTGRES_USER = "{_generate_key()}"\n'
-    content += f'POSTGRES_PASSWORD = "{_generate_key(2)}" # noqa: S105\n'
+    content += f'POSTGRES_USER = "{get_or_generate_key(name="POSTGRES_USER")}"\n'
+    content += f'POSTGRES_PASSWORD = "{get_or_generate_key(name="POSTGRES_PASSWORD", multiplier=2)}" # noqa: S105\n'
 
     postgres_env_file = path.open("w+")
     postgres_env_file.writelines(content)
@@ -47,7 +57,7 @@ def build_postgres_env(env_name: str) -> None:
 
 def build_django_secrets(env_name: str) -> None:
     """Build secrets for .django file of .env."""
-    folder: Path = _make_folder(env_name)
+    folder: Path = make_folder(env_name)
     path: Path = folder.joinpath(".django")
 
     content: str = "# General\n"
@@ -57,11 +67,11 @@ def build_django_secrets(env_name: str) -> None:
     content += 'REDIS_URL="redis://redis:6379/0"\n'
     content += "\n# Django\n"
     content += "# ------------------------------------------------------------------------------\n"
-    content += f'DJANGO_SECRET_KEY="{_generate_key()}" # noqa: S105\n'
+    content += f'DJANGO_SECRET_KEY="{get_or_generate_key(name="DJANGO_SECRET_KEY")}" # noqa: S105\n'
     content += "DJANGO_DEBUG=True\n"
     content += "IS_LOCAL=True\n"
-    content += f'CELERY_FLOWER_USER="{_generate_key()}"\n'
-    content += f'CELERY_FLOWER_PASSWORD="{_generate_key(2)}" # noqa: S105\n'
+    content += f'CELERY_FLOWER_USER="{get_or_generate_key(name="CELERY_FLOWER_USER")}"\n'
+    content += f'CELERY_FLOWER_PASSWORD="{get_or_generate_key(name= "CELERY_FLOWER_PASSWORD", multiplier=2)}" # noqa: S105\n'
 
     django_env_file = path.open("w+")
     django_env_file.writelines(content)
