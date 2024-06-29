c624436a009ba0d9f31855bbb9a5f68871e11775
Xenepix <Firenix.nex@gmail.com>
2024-06-29 13:15:30 +0200

    feat(config): finish base config

---
+++ backend/config/settings/base.py
@@ -0,0 +1,46 @@
+"""Base settings to build other settings files upon."""
+
+from pathlib import Path
+
+import environ
+
+CELERY_BROKER_URL = "redis://localhost:6379/0"
+
+ROOT_DIR = Path(__file__).resolve(strict=True).parent.parent.parent  # backend/
+APPS_DIR = ROOT_DIR
+
+# Setup env
+env = environ.Env(
+    DEBUG=(bool, False),
+)
+DEBUG = env.bool("DJANGO_DEBUG", False)
+SECRET_KEY = env("DJANGO_SECRET_KEY")
+
+
+# TIME ZONE
+# ------------------------------------------------------------------------------
+# Local time zone. Choices are
+# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+# though not all of them may be available with every OS.
+# In Windows, this must be set to your system time zone.
+TIME_ZONE = "CET"
+# https://docs.djangoproject.com/en/dev/ref/settings/#language-code
+LANGUAGE_CODE = "en-us"
+# https://docs.djangoproject.com/en/dev/ref/settings/#site-id
+SITE_ID = 1
+# https://docs.djangoproject.com/en/dev/ref/settings/#use-i18n
+USE_I18N = True
+# https://docs.djangoproject.com/en/dev/ref/settings/#use-l10n
+USE_L10N = True
+# https://docs.djangoproject.com/en/dev/ref/settings/#use-tz
+USE_TZ = True
+# https://docs.djangoproject.com/en/dev/ref/settings/#locale-paths
+LOCALE_PATHS = [str(ROOT_DIR / "locale")]
+
+# URLS
+# ------------------------------------------------------------------------------
+BASE_URL = "http://127.0.0.1:8000/"
+# https://docs.djangoproject.com/en/dev/ref/settings/#root-urlconf
+ROOT_URLCONF = "config.urls"
+# https://docs.djangoproject.com/en/dev/ref/settings/#wsgi-application
+WSGI_APPLICATION = "config.wsgi.application"
